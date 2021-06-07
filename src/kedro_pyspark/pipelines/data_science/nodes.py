0d7af3bb71701751f1e8416f26f26e8283de231c
Tomasz Gambin <tgambin@gmail.com>
2021-06-07 12:09:47 +0000

    ds pipeline

--- src/kedro_pyspark/pipelines/data_science/nodes.py
+++ src/kedro_pyspark/pipelines/data_science/nodes.py
@@ -32,6 +32,13 @@ such as:
 * Make predictions using the model
 * Evaluate the model based on its prediction
 """
+
+"""Example nodes to solve some common data science problems using PySpark,
+such as:
+* Train a machine learning model on a training dataset
+* Make predictions using the model
+* Evaluate the model based on its prediction
+"""
 import logging
 from typing import Any, Dict
 
@@ -40,9 +47,32 @@ from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 from pyspark.sql import DataFrame
 
 
-def report_count(data: DataFrame) -> None:
-    """Node for reporting the count of dataset
+def train_model(
+    training_data: DataFrame, parameters: Dict[str, Any]
+) -> RandomForestClassifier:
+    """Node for training a random forest model to classify the data.
+    The number of trees is defined in conf/project/parameters.yml
+    and passed into this node via the `parameters` argument.
+    For more information about random forest classifier with spark, please visit:
+    https://spark.apache.org/docs/latest/ml-classification-regression.html#random-forest-classifier
     """
+    classifier = RandomForestClassifier(numTrees=parameters["example_num_trees"])
+    return classifier.fit(training_data)
+
+
+def predict(model: RandomForestClassifier, testing_data: DataFrame) -> DataFrame:
+    """Node for making predictions given a pre-trained model and a testing dataset."""
+    predictions = model.transform(testing_data)
+    return predictions
+
+
+def report_accuracy(predictions: DataFrame) -> None:
+    """Node for reporting the accuracy of the predictions performed by the
+    previous node. Notice that this function has no outputs, except logging.
+    """
+    evaluator = MulticlassClassificationEvaluator(
+        labelCol="label", predictionCol="prediction", metricName="accuracy"
+    )
+    accuracy = evaluator.evaluate(predictions)
     log = logging.getLogger(__name__)
-    count = data.count()
-    log.info(f"Training data count: {count}" )
+    log.info("Model accuracy: %0.2f%%", accuracy * 100)
--- src/kedro_pyspark/pipelines/data_science/pipeline.py
+++ src/kedro_pyspark/pipelines/data_science/pipeline.py
@@ -29,14 +29,25 @@
 """Example data science pipeline using PySpark.
 """
 
+
 from kedro.pipeline import Pipeline, node
 
-from .nodes import  report_count
+from .nodes import predict, report_accuracy, train_model
 
 
 def create_pipeline(**kwargs):
     return Pipeline(
         [
-            node(report_count, ["training_data"], None),
+            node(
+                train_model,
+                inputs=["training_data", "parameters"],
+                outputs="example_classifier",
+            ),
+            node(
+                predict,
+                inputs=dict(model="example_classifier", testing_data="testing_data"),
+                outputs="example_predictions",
+            ),
+            node(report_accuracy, ["example_predictions"], None),
         ]
     )
